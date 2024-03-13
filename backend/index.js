4561a664e8c7c68a826a3b51f871f9d178755af5
captaincoro11 <captaincoro444@gmail.com>
2024-03-13 18:52:35 +0530

    done for now

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const dotenv = require('dotenv');
+const {connectDatabase} = require('./db.js');
+const Routes = require('./routes/index.js');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const app = express();
+dotenv.config();
+
+const port = 8080;
+
+
+connectDatabase(process.env.MONGO_URI);
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+
+app.use("/api/v1",Routes);
+
+app.get('/',(req,res)=>{
+    res.send("Here we go")
+})
+
+
+
+
+app.listen(port,()=>{
+    console.log(`Your server has been started on port:${port}`);
+})
+
+
+
+
+
+
+
+
+
+
+
