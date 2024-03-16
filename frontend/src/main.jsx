2cc52251f6f8150f387bcd941c5609ef0277f60c
captaincoro11 <121259483+captaincoro11@users.noreply.github.com>
2024-03-16 23:34:43 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './index.js'
+import App from './index.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
